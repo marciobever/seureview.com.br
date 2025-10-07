f700eadae1b2bc6d106b0da9cba765536edc15ee
marciobever <bevervansomarcio@gmail.com>
2025-10-08 00:27:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
+  // não usar output: 'standalone'
+  images: {
+    // como servimos estático/local, evita pipeline de otimização no servidor
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
