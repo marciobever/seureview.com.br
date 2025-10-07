a67514193f1d8a90e2310a2e68ca548f35f11588
marciobever <bevervansomarcio@gmail.com>
2025-10-08 00:50:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone", // <-- isso faz o Next gerar .next/standalone
-  // opcional, deixa o dev/CI mais silencioso:
-  // telemetry não afeta prod, mas pode desativar se quiser
-  // experimental: { instrumentationHook: false },
-  // images: { unoptimized: true }, // só se você quiser evitar Image Optimization
+  output: "standalone",
+  // images: { unoptimized: true }, // opcional
 };
 
 export default nextConfig;
