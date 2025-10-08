ceadc4e8e84a9330e032042a2f4ba873c49f9ab7
marciobever <bevervansomarcio@gmail.com>
2025-10-08 09:27:17 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // images: { unoptimized: true }, // opcional
+  // necessário pro Dockerfile copiar .next/standalone
+  output: "standalone",
+
+  // usando apenas imagens do /public; se um dia usar remotas, adicione aqui:
+  images: {
+    remotePatterns: [
+      // { protocol: "https", hostname: "m.media-amazon.com" },
+      // { protocol: "https", hostname: "cf.shopee.com.br" },
+    ],
+  },
+
+  reactStrictMode: true,
 };
+
 export default nextConfig;
